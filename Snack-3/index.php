3d6ad598b762a1ebe6efb22d653fbab6fc5f34bb
matteofab10 <matteofabio10@gmail.com>
2021-12-20 17:05:34 +0100

    fix snack-3

--- Snack-3/index.php
+++ Snack-3/index.php
@@ -42,15 +42,33 @@ $posts = [
     ],
 ];
 
-for($i = 0; $i < count(array_keys($posts)); $i++){
-  echo array_keys($posts)[$i];
-  echo "<br>";
-
-  foreach ($posts[array_keys($posts)[$i]] as $post){
-    echo $post["title"] . " : " . $post["author"] . " - " . $post["text"];
-    echo "<br>";
-  }
-  
-};
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div>
+        <?php 
+            for($i = 0; $i < count(array_keys($posts)); $i++){
+                echo array_keys($posts)[$i];
+                echo "<br>";
+              
+                foreach ($posts[array_keys($posts)[$i]] as $post){
+                  echo $post["title"] . " : " . $post["author"] . " - " . $post["text"];
+                  echo "<br>";
+                }
+                echo "<hr>";
+            };
+            
+        ?>
+        
+    </div>
+</body>
+</html>
