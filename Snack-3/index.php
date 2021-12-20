1758be1808a0a3540ed35a7006789ee914566242
matteofab10 <matteofabio10@gmail.com>
2021-12-20 17:38:26 +0100

    fix title snacks

--- Snack-3/index.php
+++ Snack-3/index.php
@@ -50,7 +50,7 @@ $posts = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack-3</title>
 </head>
 <body>
     
