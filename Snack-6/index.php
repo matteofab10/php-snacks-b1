45033e0ea555f5286bf34cf207fc3a731d934908
matteofab10 <matteofabio10@gmail.com>
2021-12-20 17:32:27 +0100

    snack-6 done

--- Snack-6/index.php
+++ Snack-6/index.php
@@ -24,3 +24,35 @@
     ];
  
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="background-color: grey; width:200px">
+        <?php 
+            for($i=0; $i < count($db["teachers"]); $i++) {
+                echo $db["teachers"][$i]["name"];
+                echo " ";
+                echo $db["teachers"][$i]["lastname"];
+                echo "<br>";
+            }
+        ?>
+    </div>
+    <div style="background-color: green; width:200px">
+        <?php 
+            for($i=0; $i < count($db["pm"]); $i++) {
+                echo $db["pm"][$i]["name"];
+                echo " ";
+                echo $db["pm"][$i]["lastname"];
+                echo "<br>";
+            }
+        ?>
+    </div>
+</body>
+</html>
